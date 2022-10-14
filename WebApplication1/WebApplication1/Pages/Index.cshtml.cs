93b8984157a0543798afc791b68bd28a55827d55
kwhynn1 <91710013+kwhynn1@users.noreply.github.com>
2022-10-14 09:56:26 +0100

    log book 2 task 1

---
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string name { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+         
+        }
+
+        public void OnPost()
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+
+                ViewData["NoName"] = "Registration Failed";
+
+            }
+            else
+            {
+              
+                ViewData["NoName"] = name + " Registration Successful";
+
+            }
+
+        }
+    }
+}
