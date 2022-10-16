ac8576fdbc45fabd03a8b03f0468a649c4ffaa2d
kwhynn1 <91710013+kwhynn1@users.noreply.github.com>
2022-10-16 14:22:32 +0100

    l

---
+++ CurrencyConverter/CurrencyConverter/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CurrencyConverter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public double GBP { get; set; }
+
+        [BindProperty]
+        public double EUR { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            EUR = 1.16;
+            GBP = 1.0;
+        }
+
+        public void OnPost()
+        {
+            if (GBP == 1.0) 
+            {
+                ViewData["Convert"] = "The Converted Amount Is " + EUR;
+            }
+            else
+            {
+                ViewData["Convert"] = "The Converted Amount Is " + GBP * EUR;
+            }
+
+        }
+    }
+}
